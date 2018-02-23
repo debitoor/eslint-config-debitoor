a926bc547541671deb956d47dce22f99b3db8aaa
Jonatan Pedersen <jonatanpedersen@users.noreply.github.com>
2018-02-23 20:59:16 +0100

    Add object rest spread (#5)

--- index.js
+++ index.js
@@ -45,6 +45,9 @@ module.exports = {
 	},
 	'parserOptions': {
 		'ecmaVersion': 2017,
-		'sourceType': 'module'
+		'sourceType': 'module',
+		'ecmaFeatures': {
+			'experimentalObjectRestSpread': true
+		}
 	}
 };
